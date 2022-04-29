dec3dee2880ccc37004e25ca66c031f8fb273b13
tparaoan <tiffany.paraoan@cloudreach>
2022-04-29 13:24:13 -0500

    Removed Database

--- security_groups.tf
+++ security_groups.tf
@@ -45,27 +45,3 @@ resource "aws_security_group" "loadbalancer_sg" {
     cidr_blocks = ["0.0.0.0/0"]
   }
 }
- resource "aws_security_group" "database-security-group" {
-  name        = "Database Security Group"
-  description = "Allow access to DB instance"
-  vpc_id      = data.aws_vpc.main_vpc.id
-
-  ingress {
-    description = "Allow EC2 instance to access RDS Database"
-    from_port   = 3306
-    to_port     = 3306
-    protocol    = "tcp"
-    security_groups = [aws_security_group.app_sg.id]
-  }
-
-  # OUTBOUD CONNECTIONS
-  egress {
-    description = "Allow access to the world"
-    from_port   = 0
-    to_port     = 0
-    protocol    = "-1" # TCP + UDP
-    cidr_blocks = ["0.0.0.0/0"]
-  }
-}
-    
-  
